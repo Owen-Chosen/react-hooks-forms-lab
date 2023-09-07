ad58bac94fa154c6bb3e672b5047c8a936154777
owen <owen.chosen@gmail.com>
2023-09-07 14:10:30 +0100

    Done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,24 +2,55 @@ import React, { useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+import { v4 as uuid } from "uuid";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [selectedName, setSelectedName] = useState("");
+  const [currentEventTarget, setCurrentEventTarget] = useState("filter");
+  const [newItem, setNewItem] = useState({
+    name: "",
+    category: "Produce",
+  });
+  
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+    setCurrentEventTarget(event.target.name);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(event) {
+    setSelectedName(event.target.value);
+    setCurrentEventTarget(event.target.name);
+  }
 
-    return item.category === selectedCategory;
+function onItemFormSubmit (name, category) {
+    setNewItem({
+      id: uuid(),
+      name: name,
+      category: category,
+    })
+  itemsToDisplay.push(newItem)
+}
+
+  const itemsToDisplay = items.filter((item) => {
+    
+    if (currentEventTarget === "search") {
+      if (selectedName === "") return true;
+      return item.name.toLowerCase().includes(selectedName.toLowerCase());
+    }
+
+    else if (currentEventTarget === "filter") {
+      if (selectedCategory === "All") return true;
+      return item.category === selectedCategory;
+    }
+   
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange = {handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
